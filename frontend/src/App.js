d8869ba3b9c9cfa2bb1c067a9378c117cc156c5f
ankurkumarhub <ankurkumar1610@gmail.com>
2020-10-14 09:35:28 +0530

    Navbar Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to MetroShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to MetroShop</h1>
+        </Container>
+      </main>
+      <Footer></Footer>
     </>
   );
 };
