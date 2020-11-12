9b59e2f572bcc8330e50b6e6d237d6e8d3727566
ankurkumarhub <ankurkumar1610@gmail.com>
2020-11-12 09:48:12 +0530

    Protecting User routes using JWT and creating User

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
 
 // @desc    Auth user & get token
 // @route   POST /api/user/login
@@ -8,14 +9,13 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
-  console.log(user);
   if (user && (await user.matchPassword(password))) {
     res.send({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user.id),
     });
   } else {
     console.log('Error');
@@ -24,4 +24,52 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user.id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('Invalid data');
+  }
+});
+
+// @desc    Get user profile
+// @route   GET /api/user/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, registerUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { authUser } from '../controller/userController.js';
-
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from '../controller/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
+router.route('/').post(registerUser);
 router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
